ac8af8e915088d83dd498696e938e39067231f54
open dev <opendev.top@gmail.com>
2024-04-16 11:34:20 +0800

    Add Navbar button focus

--- react-crash/src/components/Navbar.jsx
+++ react-crash/src/components/Navbar.jsx
@@ -1,8 +1,13 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import logo from "../assets/images/logo.png"
 
 const Navbar = () => {
+    const linkClass = ({ isActive }) =>
+        isActive
+            ? "bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+            : "text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+
     return (
         <nav className="bg-indigo-700 border-b border-indigo-500">
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -17,24 +22,15 @@ const Navbar = () => {
                         </a>
                         <div className="md:ml-auto">
                             <div className="flex space-x-2">
-                                <Link
-                                    to="/"
-                                    className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                                >
+                                <NavLink to="/" className={linkClass}>
                                     Home
-                                </Link>
-                                <Link
-                                    to="/jobs"
-                                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                                >
+                                </NavLink>
+                                <NavLink to="/jobs" className={linkClass}>
                                     Jobs
-                                </Link>
-                                <Link
-                                    to="/add-job"
-                                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                                >
+                                </NavLink>
+                                <NavLink to="/add-job" className={linkClass}>
                                     Add Job
-                                </Link>
+                                </NavLink>
                             </div>
                         </div>
                     </div>
