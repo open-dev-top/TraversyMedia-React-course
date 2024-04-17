0ed3c41eba23bca1e26cbb824581880f07808f0c
open dev <opendev.top@gmail.com>
2024-04-18 01:18:10 +0800

    Save an endpoints

--- react-crash/src/App.jsx
+++ react-crash/src/App.jsx
@@ -11,12 +11,14 @@ import JobsPage from "./pages/JobsPage"
 import NotFoundPage from "./pages/NotFoundPage"
 import JobPage, { jobLoader } from "./pages/JobPage"
 import JobListing from "./components/JobListing"
+import AddJobPage from "./pages/AddJobPage"
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<MainLayout />}>
             <Route index element={<HomePage />} />
             <Route path="/jobs" element={<JobsPage />} />
+            <Route path="/add-job" element={<AddJobPage />} />
             <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
             <Route path="*" element={<NotFoundPage />} />
         </Route>
